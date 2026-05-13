fc637a50306fe59cdf54e25918b53f74f952b9b7
Frankie Lawrence <frank4law@gmail.com>
2026-05-12 20:41:04 -0400

    Add travel route and Handlebars view

--- Module1/travlr/app_server/controllers/main.js
+++ Module1/travlr/app_server/controllers/main.js
@@ -3,6 +3,12 @@ const index = (req, res) => {
   res.render('index', { title: 'Travlr Getaways' });
 };
 
+/* GET travel page */
+const travel = (req, res) => {
+  res.render('travel', { title: 'Travlr Getaways - Travel' });
+};
+
 module.exports = {
-  index
+  index,
+  travel
 };
--- Module1/travlr/app_server/routes/index.js
+++ Module1/travlr/app_server/routes/index.js
@@ -6,4 +6,7 @@ var ctrlMain = require('../controllers/main');
 /* GET home page. */
 router.get('/', ctrlMain.index);
 
+/* GET travel page. */
+router.get('/travel', ctrlMain.travel);
+
 module.exports = router;
