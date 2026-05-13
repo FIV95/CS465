8fb277d02d186be518ec986483fc274f5fcb2ea3
Frankie Lawrence <frank4law@gmail.com>
2026-05-12 20:43:14 -0400

    Refactor travel page to use Handlebars partials

--- Module1/travlr/app_server/controllers/main.js
+++ Module1/travlr/app_server/controllers/main.js
@@ -1,11 +1,17 @@
 /* GET home page */
 const index = (req, res) => {
-  res.render('index', { title: 'Travlr Getaways' });
+  res.render('index', {
+    title: 'Travlr Getaways',
+    activeHome: true
+  });
 };
 
 /* GET travel page */
 const travel = (req, res) => {
-  res.render('travel', { title: 'Travlr Getaways - Travel' });
+  res.render('travel', {
+    title: 'Travlr Getaways - Travel',
+    activeTravel: true
+  });
 };
 
 module.exports = {
