660e2973cfd47e8a4096095292310407089a8720
Frankie Lawrence <frank4law@gmail.com>
2026-05-30 16:11:44 -0400

    Add Angular trip editing form

--- travlr/app_admin/src/app/edit-trip/edit-trip.component.ts
+++ travlr/app_admin/src/app/edit-trip/edit-trip.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Trip } from '../trip-listing/trip-listing.component';
+import { TripDataService } from '../services/trip-data.service';
 
 @Component({
   selector: 'app-edit-trip',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-trip.component.html',
   styleUrl: './edit-trip.component.css'
 })
-export class EditTripComponent {
+export class EditTripComponent implements OnInit {
+  trip: Trip = {
+    code: '',
+    name: '',
+    length: '',
+    start: '',
+    resort: '',
+    perPerson: '',
+    image: 'reef1.jpg',
+    description: ''
+  };
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private tripDataService: TripDataService
+  ) { }
+
+  ngOnInit(): void {
+    const tripCode = this.route.snapshot.paramMap.get('tripCode');
+
+    if (tripCode) {
+      this.tripDataService.getTrip(tripCode)
+        .subscribe({
+          next: (trip: Trip) => {
+            this.trip = {
+              ...trip,
+              start: trip.start ? trip.start.substring(0, 10) : ''
+            };
+          },
+          error: (err) => {
+            console.error('Error loading trip:', err);
+          }
+        });
+    }
+  }
+
+  public updateTrip(): void {
+    this.tripDataService.updateTrip(this.trip)
+      .subscribe({
+        next: () => {
+          this.router.navigate(['']);
+        },
+        error: (err) => {
+          console.error('Error updating trip:', err);
+        }
+      });
+  }
 }
