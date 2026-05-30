ecd3070d562b905b61bf4ea9f9fb8e6dedeeb93b
Frankie Lawrence <frank4law@gmail.com>
2026-05-30 16:06:38 -0400

    Add Angular edit trip route

--- travlr/app_admin/src/app/trip-card/trip-card.component.ts
+++ travlr/app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 import { Trip } from '../trip-listing/trip-listing.component';
 
 @Component({
   selector: 'app-trip-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './trip-card.component.html',
   styleUrl: './trip-card.component.css'
 })
