1850363da70fa478c205f2c5e8e619f3600a1f5f
Frankie Lawrence <frank4law@gmail.com>
2026-05-30 16:00:48 -0400

    Add Angular trip creation form

--- travlr/app_admin/src/app/add-trip/add-trip.component.ts
+++ travlr/app_admin/src/app/add-trip/add-trip.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Trip } from '../trip-listing/trip-listing.component';
+import { TripDataService } from '../services/trip-data.service';
 
 @Component({
   selector: 'app-add-trip',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-trip.component.html',
   styleUrl: './add-trip.component.css'
 })
 export class AddTripComponent {
+  newTrip: Trip = {
+    code: '',
+    name: '',
+    length: '',
+    start: '',
+    resort: '',
+    perPerson: '',
+    image: 'reef1.jpg',
+    description: ''
+  };
 
+  constructor(
+    private tripDataService: TripDataService,
+    private router: Router
+  ) { }
+
+  public addTrip(): void {
+    this.tripDataService.addTrip(this.newTrip)
+      .subscribe({
+        next: () => {
+          this.router.navigate(['']);
+        },
+        error: (err) => {
+          console.error('Error adding trip:', err);
+        }
+      });
+  }
 }
