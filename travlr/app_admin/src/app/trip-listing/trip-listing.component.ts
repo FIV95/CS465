965f92474519334fb0d5ad34a75f4a03a6d27913
Frankie Lawrence <frank4law@gmail.com>
2026-05-30 15:35:56 -0400

    Display hardcoded trips in Angular listing

--- travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
+++ travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export class Trip {
+  code!: string;
+  name!: string;
+  length!: string;
+  start!: string;
+  resort!: string;
+  perPerson!: string;
+  image!: string;
+  description!: string;
+}
 
 @Component({
   selector: 'app-trip-listing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './trip-listing.component.html',
   styleUrl: './trip-listing.component.css'
 })
 export class TripListingComponent {
-
+  trips: Trip[] = [
+    {
+      code: 'GALR210214',
+      name: 'Gale Reef',
+      length: '4 nights / 5 days',
+      start: '2024-02-14',
+      resort: 'Emerald Bay, 3 stars',
+      perPerson: '799.00',
+      image: 'reef1.jpg',
+      description: 'Gale Reef - Sed et augue lorem. In sit amet placerat arcu.'
+    },
+    {
+      code: 'DAWR210315',
+      name: 'Dawson’s Reef',
+      length: '4 nights / 5 days',
+      start: '2024-03-15',
+      resort: 'Blue Lagoon, 4 stars',
+      perPerson: '1199.00',
+      image: 'reef2.jpg',
+      description: 'Dawson’s Reef - Integer magna leo, posuere et dignissim vitae.'
+    },
+    {
+      code: 'CLAR210621',
+      name: 'Claire’s Reef',
+      length: '4 nights / 5 days',
+      start: '2024-06-21',
+      resort: 'Coral Sands, 5 stars',
+      perPerson: '1999.00',
+      image: 'reef3.jpg',
+      description: 'Claire’s Reef - Donec sed felis risus. Nulla facilisi.'
+    }
+  ];
 }
