44f8f3ce0684dc33a5f1618e8841dfda2f43edb3
Frankie Lawrence <frank4law@gmail.com>
2026-05-30 16:03:28 -0400

    Load public travel page from MongoDB

--- travlr/app_server/controllers/main.js
+++ travlr/app_server/controllers/main.js
@@ -1,9 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
-const trips = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '../../data/trips.json'), 'utf8')
-);
+const mongoose = require('mongoose');
+const Trip = mongoose.model('trips');
 
 /* GET home page */
 const index = (req, res) => {
@@ -14,12 +10,21 @@ const index = (req, res) => {
 };
 
 /* GET travel page */
-const travel = (req, res) => {
-  res.render('travel', {
-    title: 'Travlr Getaways - Travel',
-    activeTravel: true,
-    trips
-  });
+const travel = async (req, res) => {
+  try {
+    const trips = await Trip.find({}).lean().exec();
+
+    res.render('travel', {
+      title: 'Travlr Getaways - Travel',
+      activeTravel: true,
+      trips
+    });
+  } catch (err) {
+    res.status(500).render('error', {
+      message: 'Unable to retrieve trips',
+      error: err
+    });
+  }
 };
 
 module.exports = {
